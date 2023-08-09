0bb5805e57ca3012331793f89d31bbe37f2359d8
Dominic <dominiclee0512@gmail.com>
2023-08-09 17:13:02 +0800

    chore: include air-bnb lint and specify the rules for nestJS.

--- services/gateway/.eslintrc.js
+++ services/gateway/.eslintrc.js
@@ -7,9 +7,17 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'airbnb-base',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
+  settings: {
+    'import/resolver': {
+      typescript: {}
+    },
+  },
   root: true,
   env: {
     node: true,
@@ -21,5 +29,25 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'no-useless-constructor': 'off', // Useful when redeclaring DI class
+    'class-methods-use-this': 'off', // Useful when putting pure util function in DI class
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+    'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          'js': 'never',
+          'jsx': 'never',
+          'ts': 'never',
+          'tsx': 'never'
+        }
+    ],
+    "import/no-extraneous-dependencies": [
+      "error", 
+      {
+        "devDependencies": ["**/*.spec.ts"]
+      }
+    ]
   },
 };
